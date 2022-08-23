bb5dad770ff2aaa5c05b8e4f6cf9d15d20a23ecd
Tokusei Noborio <tokusei@nyampass.com>
2022-08-23 15:35:32 +0900

    refactor file structure

--- main/env.lua
+++
@@ -1,6 +0,0 @@
-local module = {}
-
-module.STONE_WIDTH = 48
-module.STONE_HEIGHT = 48
-
-return module
---
+++ main/modules/constants.lua
@@ -0,0 +1,6 @@
+local constants = {}
+
+constants.STONE_WIDTH = 48
+constants.STONE_HEIGHT = 48
+
+return constants
