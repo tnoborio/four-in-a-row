a54133ed939631352c9619717d9da509eeaaae2b
Tokusei Noborio <tokusei@nyampass.com>
2022-08-26 14:21:06 +0900

    play sound on cursor script

--- main/stones/first.go
+++ main/stones/first.go
@@ -33,26 +33,3 @@ embedded_components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "sound"
-  type: "sound"
-  data: "sound: \"/assets/tap.wav\"\n"
-  "looping: 0\n"
-  "group: \"master\"\n"
-  "gain: 1.0\n"
-  "pan: 0.0\n"
-  "speed: 1.0\n"
-  "loopcount: 0\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
