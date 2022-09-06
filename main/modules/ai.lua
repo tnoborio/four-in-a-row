7cc26daf543cdcb9def6d63f5d3b11a7b6de8330
Tokusei Noborio <tokusei@nyampass.com>
2022-09-07 08:54:10 +0900

    fix logic

--- main/modules/ai.lua
+++ main/modules/ai.lua
@@ -1,40 +1,48 @@
 local logic = require("main.modules.logic")
 
 local m = {}
+local my_turn = 1
 
 function m.ai_think()
+    my_turn = logic.turn()
+    print('my turn: ' .. my_turn)
     local start = os.time()
     pos = m.minimax(0)
     print("x: " .. pos[1] .. ", y: " .. pos[2])
-    print('time: ' .. os.time() - start)
+    -- print('time: ' .. os.time() - start)
     return pos
 end
 
 function m.my_turn()
-    return logic.turn() == 2
+    return my_turn == logic.turn()
 end
 
 function m.minimax(depth)
-    if logic.state() ~= 0 then
+    -- if depth == 2 then
+    --     print('minimax: ' .. depth .. ' ' .. logic.state())
+    --     logic.display()
+    -- end
+    -- if logic.state() ~= 0 or depth > 5 then
+    if logic.state() ~= 0 or depth > 3 then
         return m.evaluate(logic.state(), depth)
     end
     local best = {0, 0}
     local can_hand = {0, 0}
 
-    local value = m.my_turn() and 10000 or -10000
+    local value = m.my_turn() and -10000 or 10000
 
-    if depth > 3 then
-        return -value
-    end
+    -- if depth > 4 then
+    --     return -value
+    -- end
 
     -- if depth == 0 or depth == 1 then
     --     print("!! my turn: " .. (m.my_turn() and "my" or "other"))
-    --     print("   value: " .. value)
+    --     print("!! value: " .. value)
     -- end
 
     for x = 0, 6 do
         for y = 0, 5 do
-            if logic.can_put(x, y) then
+            if not logic.board(x, y) then
                 can_hand = {x, y}
                 logic.put(x, y)
                 -- if depth == 0 then
@@ -42,24 +50,15 @@ function m.minimax(depth)
                 --     -- print('state: ' .. logic.state())
                 -- end
                 child_value = m.minimax(depth + 1)
-                -- if depth == 0 then
-                --     print('-- value: ' .. value)
-                --     print('-- child value: ' .. child_value)
-                --     print('-- depth: ' .. depth)
-                --     print('-- state: ' .. logic.state())
-                --     print('-- my turn: ' .. (m.my_turn() and 'my' or 'other'))
-                --     logic.display()
-                -- end
+                if depth == 0 then
+                    print('-- value: ' .. value)
+                    print('-- child value: ' .. child_value)
+                    print('-- depth: ' .. depth)
+                    print('-- state: ' .. logic.state())
+                    print('-- my turn: ' .. (m.my_turn() and 'my' or 'other'))
+                    logic.display()
+                end
                 if m.my_turn() then
-                    if child_value > value then
-                        value = child_value
-                        best = {x, y}
-                        -- if depth == 0 then
-                        --     print('-- update: ' .. value)
-                        --     print('-- x, y: ' .. x .. ",  " .. y)
-                        -- end        
-                    end
-                else
                     if child_value < value then
                         value = child_value
                         best = {x, y}
@@ -68,11 +67,21 @@ function m.minimax(depth)
                         --     print('-- x, y: ' .. x .. ",  " .. y)
                         -- end        
                     end
+                else
+                    if child_value > value then
+                        value = child_value
+                        best = {x, y}
+                        -- if depth == 0 then
+                        --     print('-- update: ' .. value)
+                        --     print('-- x, y: ' .. x .. ",  " .. y)
+                        -- end        
+                    end
                 end
-                -- if depth == 0 or depth == 1 then
+                -- if depth == 0 then
                 --     print('-- value: ' .. value)
                 -- end
                 logic.undo(x, y)
+                break
             end
         end
     end
@@ -90,19 +99,72 @@ end
 function m.evaluate(state, depth)
     -- print("evaluate: " .. state .. " depth: " .. depth)
 
-    if state == 2 then
-        return 10 - depth
-    elseif state == 1 then
-        return depth - 10
+    if state == logic.turn() then
+        return 37 - depth
+    elseif state ~= 0 then
+        return depth - 37
     else
         return 0
     end
 end
 
--- -- logic.put(6, 0)
--- logic.display()
--- local pos = m.ai_think()
--- logic.put(pos[1], pos[2])
--- logic.display()
+function m.test_hit_1()
+    logic.init({
+        {0, 0, 0, 0, 0, 0, 2},
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0},
+        {2, 2, 2, 0, 0, 0, 0},
+    })
+    logic.put(0, 1)
+    print(logic.state())
+    logic.put(3, 0)
+    print(logic.state())
+    logic.display()
+end
+
+-- m.test_hit_1()
+
+function m.test_1()
+    logic.init({
+        {0, 0, 0, 0, 0, 0, 2},
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 0, 0, 0, 0},
+    })
+    logic.display()
+    local pos = m.ai_think()
+    logic.put(pos[1], pos[2])
+    print(logic.finished)
+end
+
+function m.test_2()
+    logic.init({
+        {0, 0, 0, 0, 0, 0, 2},
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0},
+        {2, 2, 2, 0, 0, 0, 0},
+    })
+    logic.display()
+    local pos = m.ai_think()
+    logic.put(pos[1], pos[2])
+    print(logic.finished)
+    logic.display()
+end
+
+-- m.test_2()
+
+-- logic.init()
+-- while logic.finished ~= 0 do
+--     logic.display()
+--     local pos = m.ai_think()
+--     logic.put(pos[1], pos[2])
+--     print(logic.finished)
+-- end
 
 return m
